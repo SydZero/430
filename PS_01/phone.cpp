8575273210df6f216cbfc29637d55cc18518537b
Sydney <sydferris@gmail.com>
2024-01-25 16:16:41 -0600

    finished phone

--- PS_01/phone.cpp
+++ PS_01/phone.cpp
@@ -4,10 +4,7 @@ using namespace std;
 
 
 int main(){
-    // std::ios::sync_with_stdio(false);
-    // cin.tie(NULL);
-    // cout.tie(NULL);
-    
+ 
     int t = 0;
     cin >> t;
     int n;
@@ -15,33 +12,19 @@ int main(){
     for(int i = 0; i < t; i++){
         cin >> n;
         vector<string> strs(n);
-        bool valid = true;
         
         for(int j = 0; j < n; j++){
             cin >> strs[j];
-            if(!valid){
-                continue;
-            }
-            for(int k = j - 1; k >= 0; k--){
-                if(j == k){
-                    continue;
-                }
-                string numk = strs[k];
-                string numj = strs[j];
-                //cout << numk << " " << numj << "?" << endl;
-                if(numj.length() < numk.length() && numk.find(numj) != string::npos){
-                    valid = false;
-                    //cout << "FLAG 1" << endl;
-                    break;
-                } else if(numj.length() > numk.length() && numj.find(numk) != string::npos){
-                    valid = false;
-                    //cout << "FLAG 2:" << numj << " " << numk << endl;
-                    break;
-                } else if(numj == numk){
-                    valid = false;
-                    //cout << "FLAG 3" << endl;
-                    break;
-                }
+        }
+        
+        //sorting strings does it ALPHABETICALLY, not by size lol
+        sort(strs.begin(), strs.end());
+
+        bool valid = true;
+        for(int j = 0; j < n - 1; j++){
+            if(strs[j + 1].length() > strs[j].length() && strs[j + 1].find(strs[j]) == 0){
+                valid = false;
+                break;
             }
         }
         if(valid){
@@ -49,13 +32,6 @@ int main(){
         } else {
             cout << "NO" << endl;
         }
-        
-        
-    }
-
-   
-    
-
-
+    }  
     return 0;
 }
