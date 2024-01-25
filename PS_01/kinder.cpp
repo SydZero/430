9836d81e9c29345e80d675bacf5166b1c9374d2b
Sydney <sydferris@gmail.com>
2024-01-25 16:24:26 -0600

    finished phone

--- PS_01/kinder.cpp
+++ PS_01/kinder.cpp
@@ -11,35 +11,29 @@ int main(){
 
     for(int i = 0; i < word.length(); i++){
         nums.push_back(word[i] - '0');
-        if(i > 0 && nums[i] < nums[i-1]){
-            swap(nums[i], nums[i-1]);
-            swaps++;
-        }
-    }
-
-    bool finished = false;
-    while(!finished){
-        finished = true;
-        for(int i = 1; i < nums.size(); i++){
-            int j = i;
-            if(j > 0 && nums[j] < nums[j-1]){
-                swap(nums[j], nums[j-1]);
-                swaps++;
-                finished = false;
-            }
-        }
     }
     
-    // for(int i = 1; i < nums.size(); i++){
-    //     int j = i;
-    //     int curr = nums[j];
-    //     while(j > 0 && curr < nums[j-1]){
-    //         nums[j] = nums[j-1];
-    //         swaps++;
-    //         j--;
-    //     }
-    //     nums[j] = curr;
-    // }
+    bool finished = false;
+    for(int i = nums.size() - 1; i > 0; i--){
+        int j = 0;
+        while(j < i){
+            if(nums[j] > nums[j + 1]){
+                swap(nums[j], nums[j + 1]);
+                swaps++;
+            }
+        }
+        
+    }
+        // finished = true;
+        // for(int i = 1; i < nums.size(); i++){
+        //     int j = i;
+        //     if(j > 0 && nums[j] < nums[j-1]){
+        //         swap(nums[j], nums[j-1]);
+        //         swaps++;
+        //         finished = false;
+        //     }
+        // }
+    
 
     cout << swaps << endl;
 
