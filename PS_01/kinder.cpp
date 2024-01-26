5ba4419809875eea75a44417549bd5b5b259d206
Sydney <sydferris@gmail.com>
2024-01-25 23:48:39 -0600

    all work

--- PS_01/kinder.cpp
+++ PS_01/kinder.cpp
@@ -4,14 +4,15 @@ using namespace std;
 int main(){
 
     vector<int> nums;
+    
     string word;
     cin >> word;
 
-    int swaps = 0;
+    long long swaps = 0;
 
-    int zeros = 0;
-    int ones = 0;
-    int twos = 0;
+    long long zeros = 0;
+    long long ones = 0;
+    long long twos = 0;
 
     for(int i = 0; i < word.length(); i++){
         nums.push_back(word[i] - '0');
@@ -26,35 +27,20 @@ int main(){
         }
     }
     
-    // bool finished = false;
-    // for(int i = nums.size() - 1; i > 0; i--){
-    //     int j = 0;
-    //     while(j < i){
-    //         if(nums[j] > nums[j + 1]){
-    //             swap(nums[j], nums[j + 1]);
-    //             swaps++;
-    //         }
-    //         j++;
-    //     }
-        
-    // }
-    int jtot = 0;
-    //tree loops to put 0, 1, 2 in place in order
-    for(int k = 0; k < 3; k++){
-        int j = 0;
-        for(int i = nums.size() - 1; i - j > 0; i--){
-            if(nums[i - j] == k){
-                j++;
-            }
-            if(j > 0 && i - j >= 0){
-                nums[i] = nums[i - j];
-                swaps++;
-            }
+    for(int i = 0; i < nums.size(); i++){
+        if(nums[i] == 0){
+            zeros--;
         }
-        jtot += j;
-}
-    
-
+        if(nums[i] == 1){
+            ones--;
+            swaps += zeros;
+        }
+        if(nums[i] == 2){
+            twos--;
+            swaps += zeros + ones;
+        }
+        
+    }
 
     cout << swaps << endl;
 
