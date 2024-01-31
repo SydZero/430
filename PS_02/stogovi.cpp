7c4a105695866afd7d1ce3a0988cbad4d4423d63
Sydney <sydferris@gmail.com>
2024-01-31 09:35:06 -0600

    UGGGGG

--- PS_02/stogovi.cpp
+++ PS_02/stogovi.cpp
@@ -1,6 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// ideas
+// node with i and v vals, pointer to prev stack top node
+// vector of pointers to nodes
+// set to determine different numbers in two stacks
+// singly linked list but make it gross
+
+struct node{
+    int top;
+    node* under;
+};
+
 int main(){
 
     std::ios::sync_with_stdio(false);
@@ -8,13 +19,50 @@ int main(){
     cout.tie(NULL);
 
     int n;
-
     cin >> n;
-    vector<string> strs;
-    string temp;
+
+    //some data structure, not vec of stacks
+    vector<node> nums;
+
+    char ins;
+
     for(int i = 0; i < n; i++){
-        cin >>  temp;
-        strs.push_back(temp);
+        cin >> ins;
+        int v;
+        cin >> v;
+        v--;
+        node temp_node;
+        if(ins == 'a'){
+            if(v == -1){
+                temp_node.under = nullptr;
+                
+            } else {
+                temp_node.under = &nums.at(i);
+            }
+            temp_node.top = i;
+        } else if(ins == 'b'){
+            cout << nums.at(v).top << endl;
+            nums.at(i) = *nums.at(v).under;
+        } else if(ins == 'c'){
+            set<int> unique;
+            int w;
+            cin >> w;
+            unique.insert(nums.at(v).top);
+            unique.insert(nums.at(w).top);
+            node* curr = nums.at(v).under;
+            while(curr){
+                unique.insert(curr->top);
+                curr = curr->under;
+            }
+            unique.insert(nums.at(w).top);
+            curr = nums.at(w).under;
+            while(curr){
+                unique.insert(curr->top);
+                curr = curr->under;
+            }
+            cout << unique.size() << endl;
+        }
+
     }
 
 
