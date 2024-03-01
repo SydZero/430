7da25a487b830e306b1c552e65a604d5d4f3aadf
Sydney <sydferris@gmail.com>
2024-03-01 13:33:25 -0600

    sttempts

--- PS_06/bachet.cpp
+++ PS_06/bachet.cpp
@@ -1,19 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void print(vector<bool>& v){
+    for(int i = 0; i < v.size(); i++){
+        cout << v.at(i) << " ";
+    }
+    cout << endl;
+}
+
+
 int main(){
     long long stones;
     while(cin >> stones){
-        vector<long long> available;
         long long nums;
         cin >> nums;
+        vector<bool> whowins(stones + 1, false);
         for(int i = 0; i < nums; i++){
-            long long moves;
-            cin >> moves;
-            available.push_back(moves);
+            int num;
+            cin >> num;
+            for(int j = 0; j < nums; j++){
+                if(j == 0){
+                    break;
+                }
+                if(j % num == 0){
+                    whowins[j] = whowins[j] || !whowins[j - 1];
+                }
+            }
+            if(whowins.size() < 40){
+                print(whowins);
+            }
         }
-        
-
+        cout << whowins[stones] << endl;
     }
       
     return 0;
--- PS_06/nart.cpp
+++ PS_06/nart.cpp
@@ -1,55 +1,68 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 typedef vector<vector<int>> Gallery;
 typedef vector<vector<vector<int>>> DPMat;
 
+//helloooo
+//I have been working for so longggg
+//heellllpppp
+
+//DP recursion
 int find_path(int k, int r, int c, Gallery& gallery, DPMat& dpmat){
-    cout << "  find path: " << k << " " << r << " " << c << endl;
+    //if prev row had no room closed
     if(c == INT32_MIN){
-        return min(find_path(k, r, 0, gallery, dpmat), find_path(k, r, 1, gallery, dpmat));
+        int ret = min(find_path(k, r, 0, gallery, dpmat), find_path(k, r, 1, gallery, dpmat));
+        return ret;
     }
-    if(r >= 0 && k > 0){
-        if(dpmat[k][r][c] == 0){
-            int val1 = find_path(k - 1, r - 1, c, gallery, dpmat) + gallery[r][c];
-            int val2 = find_path(k, r - 1, INT32_MIN, gallery, dpmat);
-            dpmat[k][r][c] = min(val1, val2);
-        }
-        return dpmat[k][r][c];
-    } else if(k == 0){
+    //removing nothing
+    if(k == 0){
         return 0;
     }
-    return INT32_MAX;
+
+    //if not valid row in array
+    //return val which will not be kept by min fxns
+    if(r < 0){
+        return 1000000;
+    }
+    
+    //if not already calculated
+    if(dpmat[k][r][c] != 0){
+        return dpmat[k][r][c];
+    }
+    //find min path if thisone is closed
+    int val1 = find_path(k - 1, r - 1, c, gallery, dpmat) + gallery[r][c];
+    //find min path if this is open
+    int val2 = min(find_path(k, r - 1, 0, gallery, dpmat), find_path(k, r - 1, 1, gallery, dpmat));
+
+    //min value of the options
+    dpmat[k][r][c] = min(val1, val2);
+    
+    //return partial sum  
+    return dpmat[k][r][c];
 }
 
-int main(){
-    long long n, k;
-    while(cin >> n >> k){
-        if(n == 0 && k == 0){
-            break;
-        }
-        Gallery gallery(n, vector<int>(2));
-        DPMat dpmat(k + 1, Gallery(n, vector<int>(2)));
-        for(int i = 0; i < n; i++){
-            cin >> gallery[i][0] >> gallery[i][1];
-        }
-        int sum = 0;
-        for(int i = 0; i < n; i++){
-            sum += gallery[i][0] + gallery[i][1];
-        }
 
-        for(int j = 0; j < n; j++){
-            cout << gallery[j][0] << " " << gallery[j][1] << endl;
-        }
-        cout << sum - find_path(k, n-1, INT32_MIN, gallery, dpmat) << endl;
-        for(int i = 0; i < k + 1; i++){
-            for(int j = 0; j < n; j++){
-                cout << dpmat[i][j][0] << " " << dpmat[i][j][1] << endl;
-            }
-            cout << endl;
-        }
+int main(){
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    long long n, k;
+    cin >> n >> k;
+    // if(n == 0 && k == 0){
+    //     break;
+    // }
+
+
+    Gallery gallery(n, vector<int>(2));
+    DPMat dpmat(k + 1, Gallery(n, vector<int>(2)));
+    int sum = 0;
+    for(int i = 0; i < n; i++){
+        cin >> gallery[i][0] >> gallery[i][1];
+        sum += gallery[i][0] + gallery[i][1];
     }
-      
+    cout << sum - find_path(k, n-1, INT32_MIN, gallery, dpmat) << endl;
+    long long temp;
+    cin >> temp >> temp;
     return 0;
 }
