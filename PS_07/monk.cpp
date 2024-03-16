f346af5683490fca041b3dfc8b1fbcb40dd87f30
Sydney <sydferris@gmail.com>
2024-03-15 21:19:33 -0700

    finished monk

--- PS_07/monk.cpp
+++ PS_07/monk.cpp
@@ -6,36 +6,16 @@ struct segment{
     long long y1;
     long long x2;
     long long y2;
+    double slope;
 };
 
-bool check_intersection(segment s1, segment s2){
-    long long xcoef1 = s1.y2 -  s1.y1;
-    long long ycoef1 = s1.x1 -  s1.x2;
-    long long const1 = (xcoef1 * s1.x1) + (ycoef1 * s1.y1);
-    long long xcoef2 = s2.y2 -  s2.y1;
-    long long ycoef2 = s2.x1 -  s2.x2;
-    long long const2 = (xcoef2 * s2.x1) + (ycoef2 * s2.y1);
-
-    long double det = xcoef1 * ycoef2 - xcoef2 * ycoef1;
-    if (det == 0) {
-        //cout << "Det == 0: " << s1.y1 << " " << s2.y1 << " --- " << s1.y2 << " " << s2.y2 << endl;
-        if((s1.x1 == s2.x1 && s1.y2 >= s2.y1 )|| s1.y1 == s2.y2 || s1.y2 == s2.y1){
-            cout << fixed << setprecision(6) << max(s1.x1, s2.x1) << endl;
-            return true;
+double f(double x, vector<segment>& graph){
+    for(segment s : graph){
+        if(s.x1 <= x && x <= s.x2){
+            return s.y1 + ((x - s.x1) * s.slope);
         }
-        return false;
     }
-    long double x = (ycoef2 * const1 - ycoef1 * const2) / det;
-    long double y = (xcoef1 * const2 - xcoef2 * const1) / det;
-
-    //cout << "   " << x << " " << y <<endl;
-    
-    if(x < max(s1.x1, s2.x1) || x > min(s1.x2, s2.x2)){
-        return false;
-    }
-
-    cout << fixed << setprecision(6) << x << endl;
-    return true;
+    return graph.back().y2;
 }
 
 int main(){
@@ -49,38 +29,39 @@ int main(){
     for(long long i = 0; i < a; i++){  
         long long h, t;
         cin >> h >> t;
-        acscent.push_back(segment{time, height, time + t, height + h});
+        acscent.push_back(segment{time, height, time + t, height + h, (1.0 * h) / (1.0 * t)});
         //cout << height << " " << time << " " << height + h << " " <<  time + t << endl;
         height += h;
         time += t;
     }
+    long long mheight = height;
     time = 0;
     for(long long i = 0; i < d; i++){  
         long long h, t;
         cin >> h >> t;
-        descent.push_back(segment{time, height, time + t, height - h});
-        //cout << height << " " << time << " " << height - h << " " <<  time + t << endl;
+        descent.push_back(segment{time, height, time + t, height - h, (-1.0 * h) / (1.0 * t)});
         height -= h;
         time += t;
     }
-    long long up = 0;
-    long long down = 0;
 
-    while(down < descent.size() && up < acscent.size()){
-        if(descent.at(down).y1 > acscent.at(up).y1 && descent.at(down).y2 > acscent.at(up).y2){
-            up++;
-            down++;
+    double left = 0;
+    double right = DBL_MAX;
+    double m = (left + right) / 2.0;
+
+    while(abs(left - right) > .0000001){
+        m = ((left + right) / 2.0);
+        
+        double a = f(m, acscent);
+        double d = f(m, descent);
+        //cout << fixed << setprecision(12) << "TESTING: " << m << endl;
+        //cout << a << " " << d << endl;
+        if(a >= d) {
+            right = m;
         } else {
-            if(check_intersection(descent.at(down), acscent.at(up))){
-                break;
-            } else if(descent.at(down).x2 > acscent.at(up).x2){
-                up++;
-            } else {
-                down++;
-            }
+            left = m;
         }
     }
-
-
+    cout << fixed << setprecision(6) << left << endl;
+    
     return 0;
 }
--- PS_07/question.cpp
+++ PS_07/question.cpp
@@ -8,24 +8,11 @@ void print(long long* arr, long long n){
     cout << endl;
 }
 
-void print_2d(long long** arr, long long n){
-    for(int r = 0; r < n; r++){
-        for(int c = 0; c < n; c++){
-            if(arr[r][c] == -100){
-                cout << " - ";
-            } else {
-                cout << arr[r][c] << " ";
-            }
-        }
-        cout << endl;
-    }
-    cout << endl;
-}
-
 int main(){
     long long n, m;
     cin >> n >> m;
     long long sum_cals[n][n] = {-100};
+    long long nice_cals[n][n] = {-100};
     long long cals[n] = {-100};
     long long meals[n] = {-100};
     cals[0] = m;
@@ -37,8 +24,10 @@ int main(){
     }
 
     for(int r = 0; r < n; r++){
+        // cout << cals[r] << " ";
         for(int c = n - 1; c >= 0; c--){
             if(r > c){
+                sum_cals[r][c] = 0;
                 continue;
             }
             if(c < n - 1){
@@ -46,9 +35,18 @@ int main(){
             } else {
                 sum_cals[r][c] = min(cals[c - r], meals[c]);
             }
+            nice_cals[r][c] = min(cals[c - r], meals[c]);
         }
-        //print(sum_cals[r], n);
     }
+    // cout << endl;
+
+    // for(int r = 0; r < n; r++){
+    //     print(sum_cals[r], n);
+    // }
+    // cout << "_________" << endl;
+    // for(int r = 0; r < n; r++){
+    //     print(nice_cals[r], n);
+    // }
     
     long long sum = sum_cals[0][0];
 
